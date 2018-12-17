de06cd4990ca888d47d7ff7f5f2e184ee91350af
adikrasimirowa <adi_krasimirowa@abv.bg>
2018-12-17 03:36:08 +0200

    Create task2_func.cpp

---
+++ week8/task2_func.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+ 
+void Matrix(int arr[], int n)
+{  
+   for(int i=0 ; i<n; i++)
+   {
+       cin>>arr[i];
+   }
+    
+}
+
+void EvenOdd(int arr[], int n)
+{    Matrix(arr,n);
+    int odd=0, even=0;
+    for(int i=n-1 ; i>=0; i--)
+   {
+       if(arr[i]%2 == 0)
+       {
+           even++;
+       }
+       else
+       {
+            odd++;
+       }
+   }
+   
+   cout<<"Even - >"<<even<<endl;
+   cout<<"Odd - >"<<odd<<endl;
+}
+int main()
+{
+    int arr[100];
+    int n;
+    cin>>n;
+   
+    EvenOdd(arr,n);
+}
