23c6b888610854e36ba075a4f63b2770c2679c51
adikrasimirowa <adi_krasimirowa@abv.bg>
2018-12-17 02:47:16 +0200

    Create task4.cpp

---
+++ week8/task4.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool CheckPrime(int number)
+{
+    bool isPrime = true;
+    
+    for(int i = 2; i<number; i++)
+    {
+         if(number%i == 0)
+         {
+             isPrime = false;
+             break;
+         }
+    }
+    
+    return isPrime;
+}
+
+void Sdvoini( int n, int m )
+{
+
+    if( CheckPrime(n) && CheckPrime(m) )
+    {
+        if(n+2 == m)
+        { 
+            cout<<"True";
+        }
+        else
+        {
+            cout<<"False";
+        }
+    }
+}
+
+int main()
+{
+   int n,m;
+
+  cin>>n>>m;
+  
+  Sdvoini(n,m);
+  
+}
